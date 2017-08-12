15f8868b83763e9b9b1cb0f6c5876df04fa950f4
davidmgudeman <davidmgudeman@gmail.com>
2017-08-12 07:32:27 -0700

    The cell static info was hooked up

--- RainyShinyCloudy/WeatherVC.swift
+++ RainyShinyCloudy/WeatherVC.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
-
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
+  
+  @IBOutlet weak var dateLbl: UILabel!
+  @IBOutlet weak var currentTempLbl: UILabel!
+  @IBOutlet weak var locationLbl: UILabel!
+  @IBOutlet weak var currentWeatherImage: UIImageView!
+  @IBOutlet weak var currentWeatherLbl: UILabel!
+  @IBOutlet weak var tableView: UITableView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    tableView.delegate = self
+    tableView.dataSource = self
   
   }
-
   
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 6
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+    return cell
+    
+  }
 }
 
