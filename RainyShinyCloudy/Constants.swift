fc3f816444f2b6bbb3be5a3141f1e5aad9d729c1
davidmgudeman <davidmgudeman@gmail.com>
2017-08-12 09:22:54 -0700

    added a constants field and a model

---
+++ RainyShinyCloudy/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RainyShinyCloudy
+//
+//  Created by David Gudeman on 8/12/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATTITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let APP_KEY = "d162d6f35112972011a622f68f572859"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATTITUDE)37.4419\(LONGITUDE)122.1430\(APP_ID)\(APP_KEY)"
