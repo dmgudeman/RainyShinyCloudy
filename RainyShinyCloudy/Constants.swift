afc9aa6d2e8327d8384bdafb86e83010bbbc2c52
davidmgudeman <davidmgudeman@gmail.com>
2017-08-13 12:13:57 -0700

    forecast data successfully downloading

--- RainyShinyCloudy/Constants.swift
+++ RainyShinyCloudy/Constants.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let BASE_URL = "http://api.openweathermap.org/data/2.5/"
+let WEATHER = "weather?"
 let LATTITUDE = "lat="
 let LONGITUDE = "&lon="
 let APP_ID = "&appid="
@@ -16,4 +17,9 @@ let APP_KEY = "d162d6f35112972011a622f68f572859"
 
 typealias DownloadComplete = () -> ()
 
-let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATTITUDE)37.4419\(LONGITUDE)122.1430\(APP_ID)\(APP_KEY)"
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(WEATHER)\(LATTITUDE)37.4419\(LONGITUDE)122.1430\(APP_ID)\(APP_KEY)"
+
+
+
+let FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=d162d6f35112972011a622f68f572859"
+
--- RainyShinyCloudy/WeatherVC.swift
+++ RainyShinyCloudy/WeatherVC.swift
@@ -20,6 +20,8 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
   @IBOutlet weak var tableView: UITableView!
   
   var currentWeather: CurrentWeather!
+  var forecast: Forecast!
+  var forecasts = [Forecast]()
 
   
   override func viewDidLoad() {
@@ -27,12 +29,37 @@ class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     tableView.delegate = self
     tableView.dataSource = self
+    
     currentWeather = CurrentWeather()
+  
+    
     currentWeather.downloadWeatherDetails {
       // setup ui to load downloaded data
-      self.updateMainUI()
-    }
+      self.downloadForecastData {
+        self.updateMainUI()
+      }
+          }
   
+  }
+  
+  func downloadForecastData(completed: DownloadComplete) {
+    //Downloading forecast weather data for Tableview
+    Alamofire.request(FORECAST_URL).responseJSON {response in
+      let result = response.result
+      if let dict = result.value as? Dictionary<String, AnyObject> {
+        if let list = dict["list"] as? [Dictionary<String, AnyObject>] {
+          for obj in list {
+            let forecast = Forecast(weatherDict:obj)
+            self.forecasts.append(forecast)
+            print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH \( obj)")
+          }
+        }
+        
+      }
+    
+    }
+     completed()
+    
   }
   
   func numberOfSections(in tableView: UITableView) -> Int {
